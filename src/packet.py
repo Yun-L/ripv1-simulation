f2bb25466ec1eed875db72c47370be9bf5fe6d4a
Yun-L <yun26603020@gmail.com>
2022-04-14 23:35:29 -0400

    packet command and body entries

--- src/packet.py
+++ src/packet.py
@@ -1,11 +1,29 @@
+from enum import Enum, auto
+
+class Command(Enum):
+    REQUEST = auto()
+    RESPONSE = auto()
+    TRACEON = auto()            # obsolete
+    TRACEOFF = auto()           # obsolete
+    RESERVED = auto()           # unused, only for Sun Microsystems
+
+
+class PacketBodyEntry:
+    """ Represents the repeating portion of a RIP protocol packet """
+
+    def __init__(self, address_family, ip, metric):
+        self.address_family = address_family  # ip is 2
+        self.ip = ip
+        self.metric = metric
+
+
 class Packet:
     """ Represents a single RIP protocol packet """
 
-    def __init__(self, command, version, address_family, ip, metric) -> None:
+    def __init__(self, command: int, version: str, packet_body: List[Type[PacketBodyEntry]]) -> None:
         self.command = command
         self.version = version
         self.address_family = address_family
         self.ip = ip
         self.metric = metric
-        
         
