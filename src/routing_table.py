f7be70098c330a4bfb449c24ff066496323bdc83
Yun-L <yun26603020@gmail.com>
2022-04-14 23:36:08 -0400

    add type info

--- src/routing_table.py
+++ src/routing_table.py
@@ -5,22 +5,27 @@ class RoutingTableEntry:
 
     def __init__(self, dest_name: str, dest_ip: str, metric: int,
                  next_gw_ip: str, rc_flag: bool, timers: int) -> None:
-        self.dest_name = dest_name
-        self.dest_ip = dest_ip
-        self.metric = metric
-        self.next_gw_ip = next_gw_ip
-        self.rc_flag = rc_flag
-        self.timers = timers
+        self.dest_name: str = dest_name
+        self.dest_ip: str = dest_ip
+        self.metric: int = metric  # hop count
+        self.next_gw_ip: str = next_gw_ip
+        self.rc_flag: bool = rc_flag
+        self.timers: int = timers
 
 
 class RoutingTable:
     """ RIP routing table """
 
     def __init__(self) -> None:
-        self.entry_list: List[Type[RoutingTableEntry]] = []
+        self.entry_table: Dict[str, Type[RoutingTableEntry]] = {}
 
     def add_entry(self, entry: Type[RoutingTableEntry]) -> None:
-        self.entry_list.append(entry)
+        if entry.dest_ip in self.entry_table:
+            if entry.metric == self.entry_table[entry.dest_ip].metric:
+                # if hops are same than add both
+                print("hops are the same")
+        self.entry_table[entry.dest_ip] = entry
+        return
 
     def remove_entry(self):
         return
